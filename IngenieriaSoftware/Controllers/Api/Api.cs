f96bc3d45dcec21a8e2011eac6235653bb146c6c
Alejandro <alejandro.diaz@mail.udp.cl>
2022-05-19 01:51:21 -0400

    Funcion eliminar y sweetalerts

---
+++ IngenieriaSoftware/Controllers/Api/Api.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Transactions;
+using IngenieriaSoftware.Data;
+
+namespace IngenieriaSoftware.Controllers.Api
+{
+    [Route("api")]
+    public class Api : Controller
+    {
+        protected readonly AppDbContext context;
+        public Api(AppDbContext db)
+        {
+            context = db;
+        }
+        [Route("eliminar-producto/{idProducto}/")]
+        public async Task<IActionResult> EliminarProducto(int idProducto) {
+            try
+            {
+                var idproductodebug = idProducto;
+                var productoAEliminar = context.producto.Where(p => p.id == idProducto).FirstOrDefault();
+                context.producto.Remove(productoAEliminar);
+                await context.SaveChangesAsync();
+                return Ok("Producto Eliminado: " + productoAEliminar.nombre_producto);
+            }
+            catch (Exception ex) {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
---
+++ IngenieriaSoftware/wwwroot/js/TablaProductos/eliminar-producto.js
@@ -0,0 +1,22 @@
+﻿const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+async function handleEliminarProductoSuccess(successData, idProducto) {
+    console.log('handleEliminarDestinatarioSuccess()')
+    console.log(successData)
+    Swal.fire({
+        icon: 'success',
+        title: 'Producto Eliminado',
+        text: 'Se recargará la página en instantes'
+    })
+    await sleep(2000)
+    window.location.reload()
+}
+
+const eliminarProducto = async element => {
+    console.log(">>eliminarProducto")
+    var idProducto = element.dataset.idproducto
+    console.log(element.dataset.idproducto)
+    await fetch(`/api/eliminar-producto/${idProducto}`, {
+        method: 'POST'
+    }).then(successData => handleEliminarProductoSuccess(successData, idProducto))
+}
