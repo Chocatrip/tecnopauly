2a6526ba95e368e393d33bd6f8b08cd42e3853d6
Alejandro <alejandro.diaz@mail.udp.cl>
2022-05-18 22:11:36 -0400

    Cambios modal a componentes

--- IngenieriaSoftware/Controllers/LoginController.cs
+++ IngenieriaSoftware/Controllers/LoginController.cs
@@ -6,27 +6,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Web;
 
 namespace IngenieriaSoftware.Controllers
 {
     public class LoginController : Controller
     {
         protected readonly AppDbContext context;
-        Blazored.SessionStorage.ISessionStorageService sessionStorage;
         public LoginController(AppDbContext db) {
             context = db;
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
-        [HttpPost("Login")]
-        public async Task<ActionResult> LoginControl([FromForm] IngenieriaSoftware.Models.LoginModel model)
+        [HttpPost("")]
+        public IActionResult Login([FromForm] IngenieriaSoftware.Models.LoginModel model)
         {
             if (!String.IsNullOrEmpty(model.user) && !String.IsNullOrEmpty(model.password))
             {
                 var query = context.cuenta.AsQueryable();
-                var tipocuenta = 1;
+                var tipocuenta = query.Where(q => String.Equals(q.username, model.user)).FirstOrDefault().tipocuenta;
                 var userdebug = model.user;
                 var passdebug = model.password;
                 var test = context.cuenta.Where(c => String.Equals(c.username, model.user)).Select(c => c.username).FirstOrDefault();
@@ -35,22 +32,14 @@ namespace IngenieriaSoftware.Controllers
                 {
                     if (query.Where(q => (String.Equals(q.username, model.user) && String.Equals(q.pass, model.password))).Any())
                     {
-
-                        var userObject = query
-                            .Where(q => String.Equals(q.username, model.user))
-                            .Select(q => new
-                            {
-                                username = q.username,
-                                pass = q.pass,
-                                tipocuenta = q.tipocuenta
-                            }).FirstOrDefault();
-
-                        await sessionStorage.SetItemAsync("Username", userObject.username);
-                        await sessionStorage.SetItemAsync("TipoCuenta", userObject.tipocuenta.ToString());
+                        CookieOptions options = new CookieOptions();
+                        options.Expires = DateTime.Now.AddHours(3);
+                        Response.Cookies.Append("userInfo", tipocuenta.ToString(), options);
+                        
                     }
                 }
             }
-            return View();
+            return View("~/Views/Login/Index.cshtml");
         }
     }
 }
