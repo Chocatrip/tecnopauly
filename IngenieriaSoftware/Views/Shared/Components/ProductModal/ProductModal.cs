12c4be65e31d495796842c251758392eaf759956
darkiox <mrdarkiox@gmail.com>
2022-05-19 03:07:56 -0400

    Agregar ProductModal

---
+++ IngenieriaSoftware/Views/Shared/Components/ProductModal/ProductModal.cs
@@ -0,0 +1,25 @@
+﻿using IngenieriaSoftware.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace IngenieriaSoftware.Views.Shared.Components.ProductModal
+{
+    public class ProductModal : ViewComponent
+    {
+        protected readonly AppDbContext context;
+        public ProductModal(AppDbContext db)
+        {
+            context = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+
+    }
+}
