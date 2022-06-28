cc966216ab3fe853b80c1949e0d697acbd6ca8a6
Alejandro <alejandro.diaz@mail.udp.cl>
2022-06-28 00:54:41 -0400

    Validacion no ingresar producto con mismo codigo.

--- IngenieriaSoftware/wwwroot/js/Ingresar/ingresar.js
+++ IngenieriaSoftware/wwwroot/js/Ingresar/ingresar.js
@@ -1,4 +1,16 @@
-﻿
+﻿async function ingresarfailure(xhr, err, a, b) {
+    var error = check_cookie_name("errorIngresarProducto")
+    error = error.replaceAll('-', ' ')
+    $('#ingresar-productos-tab-form').removeClass('sk-loading')
+    Swal.fire({
+        customClass: {
+            container: 'my-swal'
+        },
+        icon: 'error',
+        title: 'Error',
+        text: error
+    })
+}
 async function ingresarsuccess() {
     console.log("Trigger success");
     $('#ingresar-productos-tab-form').removeClass('sk-loading')
@@ -24,4 +36,14 @@ const pnPreventSubmitOnEnter = (e) => {
 function ingresarbegin(){
     console.log("Begin ingresar");
     $('#ingresar-productos-tab-form').addClass('sk-loading')
+}
+
+function check_cookie_name(name) {
+    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) {
+        return match[2]
+    }
+    else {
+        console.log('--something went wrong---');
+    }
 }
