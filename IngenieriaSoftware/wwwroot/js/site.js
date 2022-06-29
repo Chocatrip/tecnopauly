b581b92008a5d50cb4a6bd025d52691818141e3d
Darío <dario.marmie@mail.udp.cl>
2022-06-29 13:39:48 -0400

    Frontend Cotizaciones Historialdeventas

--- IngenieriaSoftware/wwwroot/js/site.js
+++ IngenieriaSoftware/wwwroot/js/site.js
@@ -4,4 +4,14 @@
         "scrollCollapse": true,
         "paging": true
     });
+    $('#tablaVentas').DataTable({
+        "scrollY": "450px",
+        "scrollCollapse": true,
+        "paging": true
+    });
+    $('#tablaCotizaciones').DataTable({
+        "scrollY": "450px",
+        "scrollCollapse": true,
+        "paging": true
+    });
 });
